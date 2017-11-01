77aa35d07442bec91c82cb3d8acfcf62f98a1be5
Hedonist74 <oleg.sergeevich3991@gmail.com>
2017-11-01 21:32:30 +0200

    lession 20

--- app/js/init.js
+++ app/js/init.js
@@ -38,12 +38,56 @@ $( function() {
 
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
-
+        $(target).bPopup({
+            closeClass: 'close'
+        });
     })
 
     // Form Element Styler init
 
     $('select').styler();
+
+    // scrollbar init
+
+    $('.scrollbar-inner').scrollbar();
+
+    // chart init
+
+    var chartBlock = document.getElementById('myChart');
+    if( chartBlock ){
+        var myChart = new Chart(chartBlock, {
+            type: 'bar',
+            data: {
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets: [
+                    {
+                        label:'Signups',
+                        data: [100, 200, 220, 170, 130, 90, 156, 180, 130],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label:'FTD',
+                        data: [200, 120, 180, 142, 121, 98, 170, 136, 157],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    },
+                    {
+                        label:'Earned',
+                        data: [210, 130, 170, 132, 131, 88, 180, 146, 147],
+                        backgroundColor: '#59b66d'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
+    }
 
 } );
